965655239b4106359cfc03c94246a0639d97fc66
Dave Lusty <35692266+dalusty@users.noreply.github.com>
2024-03-18 09:48:23 +0000

    user check

---
+++ index.js
@@ -0,0 +1,17 @@
+//Set up on load events
+window.addEventListener('DOMContentLoaded', pageFirstLoad, false);
+
+function pageFirstLoad() {
+    if (userinfo = getUserInfo()) {
+        document.getElementById("loginbox").innerHTML = userinfo.userDetails;
+    } else {
+        document.getElementById("loginbox").innerHTML = "No user info";
+    }
+}
+
+async function getUserInfo() {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
+}
