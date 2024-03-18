09bd062546047ac462dba0f34efb9967801f2b09
Dave Lusty <35692266+dalusty@users.noreply.github.com>
2024-03-18 10:22:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', pageFirstLoad, false);
 
 async function pageFirstLoad() {
     userinfo = await getUserInfo();
-    if (userInfo.clientPrincipal != null) {
+    if (userInfo != null) {
         document.getElementById("loginbox").innerHTML = userinfo.clientPrincipal.userDetails + " <a href=\"/.auth/logout\">(Logout)</a>";
     } else {
         document.getElementById("loginbox").innerHTML = "<a href=\"/.auth/login/aadb2c\">Login</a>";
