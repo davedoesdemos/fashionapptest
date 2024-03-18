b2651f5971226a13a99f9e615cb334341e62b4e4
Dave Lusty <35692266+dalusty@users.noreply.github.com>
2024-03-18 09:55:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ async function getUserInfo() {
     const response = await fetch('/.auth/me');
     const payload = await response.json();
     const { clientPrincipal } = payload;
-    return clientPrincipal;
+    return payload;
 }
